3b7b4a7ca71991b1e7473f3e1eaf2ac3403d787d
Michael Riley <michaelriley@situsamc.com>
2023-01-24 00:49:17 -0700

    More snippets

--- src/routes/snippets/snippets.js
+++ src/routes/snippets/snippets.js
@@ -69,6 +69,15 @@ The Great Frames of Reference Translated from the Pali by Thanissaro Bhikkhu
 </a>
 `;
 
+let rightMeditation = `
+The Four Jhanas
+<br />
+<br />
+<a href="https://cdn.britannica.com/primary_source/gutenberg/PGCC_classics/canon/digha/dn22.html">
+The Great Frames of Reference Translated from the Pali by Thanissaro Bhikkhu
+</a>
+`;
+
 let yamas = `
 <blockquote>
 The Commandments are these: noninjury, truthfulness, abstaining from stealing, from impurity, 
@@ -91,15 +100,54 @@ The Yoga Sutras of Patanjali translated by Charles Johnston
 </a>
 `;
 
+let asana = `
+<blockquote>
+Right poise must be firm and without strain. Right poise is to be gained by steady and temperate effort, and by setting the heart upon the everlasting.
+The fruit of right poise is the strength to resist the shocks of infatuation or sorrow.
+</blockquote>
+<br />
+<a href="https://www.gutenberg.org/cache/epub/2526/pg2526-images.html">
+The Yoga Sutras of Patanjali translated by Charles Johnston
+</a>
+`
+
+let pranayama = `
+<blockquote>
+The life-current is either outward, or inward, or balanced; it is regulated according to place, time, number; it is prolonged and subtle.
+</blockquote>
+<br />
+<a href="https://www.gutenberg.org/cache/epub/2526/pg2526-images.html">
+The Yoga Sutras of Patanjali translated by Charles Johnston
+</a>
+`
+
+
+let pratyahara = `
+<blockquote>
+The fourth degree transcends external and internal objects. Thereby is worn away the veil which covers up the light.
+Thence comes the mind’s power to hold itself in the light.
+</blockquote>
+<br />
+<a href="https://www.gutenberg.org/cache/epub/2526/pg2526-images.html">
+The Yoga Sutras of Patanjali translated by Charles Johnston
+</a>
+`
+
+;
+
 export const wordDict = [
     {id:'node3',title: 'Right View', text: rightView},
     {id:'node4',title: 'Right Living', text:yamas},
     {id:'node5',title: 'Right Resolve', text:rightResolve},
     {id:'node6',title: 'Right Effort', text:niyamas},
     {id:'node7',title: 'Right Speech', text:rightSpeech},
+    {id:'node8',title: 'Right Posture', text:asana},
     {id:'node9',title: 'Right Action', text:rightAction},
+    {id:'node10',title: 'Right Breath', text:pranayama},
     {id:'node11',title: 'Right Livelihood', text:rightLivelihood},
+    {id:'node12',title: 'Right Withdrawal', text:pratyahara},
     {id:'node13',title: 'Right Effort', text:rightEffort},
-    {id:'node15',title: 'Right Mindfulness', text:rightMindfulness}
+    {id:'node15',title: 'Right Mindfulness', text:rightMindfulness},
+    {id:'node17',title: 'Right Meditation', text:rightMeditation}
 
 ]

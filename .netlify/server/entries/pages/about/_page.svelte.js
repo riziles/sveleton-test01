1758a09a14a415615706cc8758bcf32048e0470e
Michael Riley <michaelriley@situsamc.com>
2023-01-24 00:49:56 -0700

    netlfied

--- .netlify/server/entries/pages/about/_page.svelte.js
+++ .netlify/server/entries/pages/about/_page.svelte.js
@@ -1,11 +1,17 @@
 import { c as create_ssr_component } from "../../../chunks/index2.js";
 const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="${"container mx-auto p-8 space-y-8"}"><h1>Disclaimer</h1>
+  return `<div class="${"grid grid-cols-2"}"><div class="${"container"}"><h1>Disclaimer</h1>
 	<p>There is no original work here, just standing on the shoulders of giants.</p>
-	<hr>
 	<section class="${"flex space-x-2"}"><a class="${"btn btn-filled-primary"}" href="${"https://kit.svelte.dev/"}" target="${"_blank"}" rel="${"noreferrer"}">SvelteKit</a>
 		<a class="${"btn btn-filled-secondary"}" href="${"https://tailwindcss.com/"}" target="${"_blank"}" rel="${"noreferrer"}">Tailwind</a>
-		<a class="${"btn btn-filled-tertiary"}" href="${"https://github.com/"}" target="${"_blank"}" rel="${"noreferrer"}">GitHub</a></section></div>`;
+		<a class="${"btn btn-filled-tertiary"}" href="${"https://github.com/"}" target="${"_blank"}" rel="${"noreferrer"}">GitHub</a></section></div>
+
+
+
+
+
+<div class="${"container"}">Some other stuff
+</div></div>`;
 });
 export {
   Page as default
--- .netlify/server/nodes/3.js
+++ .netlify/server/nodes/3.js
@@ -2,7 +2,7 @@
 
 export const index = 3;
 export const component = async () => (await import('../entries/pages/about/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/about/_page.svelte-09f06598.js';
-export const imports = ["_app/immutable/components/pages/about/_page.svelte-09f06598.js","_app/immutable/chunks/index-5db496a7.js"];
+export const file = '_app/immutable/components/pages/about/_page.svelte-ba8e50c0.js';
+export const imports = ["_app/immutable/components/pages/about/_page.svelte-ba8e50c0.js","_app/immutable/chunks/index-5db496a7.js"];
 export const stylesheets = [];
 export const fonts = [];
